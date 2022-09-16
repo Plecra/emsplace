5bd744dffecf39c4b02124c13a8a6e065133e6d6
Marli Frost <marli@frost.red>
2022-09-16 17:40:00 +0100

    add main HTML page embedding canvas

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+const fs = require("fs");
 const { ServerResponse, IncomingMessage, createServer } = require("http");
 const jpeg = require("jpeg-js");
 
+const indexhtml = fs.readFileSync("public/index.html");
+
 const SIZE = 100;
 const canvas = Buffer.alloc(SIZE * SIZE * 4);
+for (let i = 0; i < SIZE; i++) {
+    canvas[4 * (SIZE + 1) * i] = 255;
+}
 let cachedjpegdata = buffertojpeg(canvas, SIZE, SIZE);
 
 createServer(request_handler).listen(80);
@@ -13,9 +19,14 @@ createServer(request_handler).listen(80);
  */
 function request_handler(request, response) {
     switch (request.url) {
+        case "/":
+            response.setHeader("Content-Type", "text/html");
+            response.end(indexhtml);
+            break;
         case "/image":
             response.setHeader("Content-Type", "image/jpeg");
             response.end(cachedjpegdata);
+            break;
         default:
             response.statusCode = 404;
             response.end(`"place.frost.red${request.url}" not found on this server`);
@@ -23,5 +34,5 @@ function request_handler(request, response) {
 }
 
 function buffertojpeg(data, width, height) {
-    return jpeg.encode({ data, width, height }).data
+    return jpeg.encode({ data, width, height }, 100).data
 }
