08f42959b7c1fc0f301f49dceb90573f4c4fc63e
Marli Frost <marli@frost.red>
2022-09-20 20:02:29 +0100

    document the less obvious parts of the code

--- easel.js
+++ easel.js
@@ -1,11 +1,12 @@
 import jpeg from "jpeg-js";
 
+// Create a bytebuffer of RGBA values to store the canvas
 const SIZE = 24;
 const data = Buffer.alloc(SIZE * SIZE * 4);
 let cached_jpeg = make_jpeg(data);
 
 export default {
-    pipe_canvas(writable) {
+    pipe_canvas_jpeg(writable) {
         writable.end(cached_jpeg);
     }
 }
--- main.js
+++ main.js
@@ -4,9 +4,10 @@ import easel from "./easel.js"
 import webviewer from "./webviewer.js"
 
 http.createServer((req, res) => {
-    if (req.url === "/api/canvas") {
+    // This routes requests to the HTML or image generation endpoints
+    if (req.method === "GET" && req.url === "/api/canvas") {
         res.setHeader("Content-Type", "image/jpeg");
-        easel.pipe_canvas(res);
+        easel.pipe_canvas_jpeg(res);
     } else {
         webviewer(req, res);
     }
